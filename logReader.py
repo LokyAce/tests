fd18e3569e0ec7e964a1c12c78a4ace9e8777523
Yevhen Kharinskyi <lokyace+git@gmail.com>
2025-02-06 18:10:01 -0600

    lintered in PyCharm.

--- logReader.py
+++ logReader.py
@@ -1,13 +1,15 @@
-import sys, time, re, argparse
+import re, argparse, json
 from datetime import datetime
+from datetime import date
 from collections import Counter
 
-#Parsing paramters, setting defaults
+#Parsing parameters, setting defaults
 LOG_FILE_NAME = 'access.log'
 GRANULARITY = 'min'
 METHOD = 'RegExp'
 log_dict = {}
 log_counts = Counter()
+data = []
 
 parser = argparse.ArgumentParser(usage='%(prog)s logfile granularity method')
 parser.add_argument('log_file', nargs='?', help='path to a file with logs', default=LOG_FILE_NAME)
@@ -18,47 +20,48 @@ log_file_name = args.log_file
 granularity = args.granularity
 method = args.method
 
-def File_to_List(filename):
+def file_to_list(filename):
     with open(filename, 'r') as f:    
         return f.readlines()
 
-def File_to_Str(filename):
+def file_to_str(filename):
     with open(filename, 'r') as f:
         return f.read()
 
-def ProcessLogRegExp (file_name, precision): #RegExp only method
-    fileLines = File_to_List(file_name)
-    #filestr = File_to_Str(file_name)
+def process_log_regexp (file_name, precision): #RegExp only method
+    file_lines = file_to_list(file_name)
+    #filestr = file_to_str(file_name)
     pattern = r'(\d+\.\d+\.\d+\.\d+) - - \[(.*?)\] (.*)' 
-    for logLine in fileLines:
+    for logLine in file_lines:
         #print (logLine)
         match = re.match(pattern, logLine)    
         if match:
             ip, date_time, other = match.groups()
             #print (ip, '\n', date_time, '\n', other, '\n',)                    #DEBUG   
             if precision == 'sec':
-                datePattern = r'(\d{2}/[A-Za-z]{3}/\d{4}):(\d{2}:\d{2}:\d{2})'
+                date_pattern = r'(\d{2}/[A-Za-z]{3}/\d{4}):(\d{2}:\d{2}:\d{2})'
             if precision == 'min':
-                datePattern = r'(\d{2}/[A-Za-z]{3}/\d{4}):(\d{2}:\d{2})'
+                date_pattern = r'(\d{2}/[A-Za-z]{3}/\d{4}):(\d{2}:\d{2})'
             if precision == 'hour':
-                datePattern = r'(\d{2}/[A-Za-z]{3}/\d{4}):(\d{2})'
-            date_match = re.match(datePattern, date_time)
+                date_pattern = r'(\d{2}/[A-Za-z]{3}/\d{4}):(\d{2})'
+            date_match = re.match(date_pattern, date_time)
             if date_match:
-                logRecDate, logRecTime = date_match.groups()
-                #print (logRecDate, '\n', logRecTime, '\n')                     #DEBUG
-                log_dict[logRecDate+'_'+logRecTime] = ip+other
-                log_counts[logRecDate+'_'+logRecTime] += 1
+                log_rec_date, log_rec_time = date_match.groups()
+                #print (log_rec_date, '\n', log_rec_time, '\n')                     #DEBUG
+                log_dict[log_rec_date+'_'+log_rec_time] = ip+other
+                log_counts[log_rec_date+'_'+log_rec_time] += 1
 
         #print('Dict: ', log_dict, '\n')                                        #DEBUG
         #print ('Counter ', log_counts)                                         $DEBUG
 
-def ProcessLogDateObject (file_name, precision): #DateObject method
-    fileLines = File_to_List(file_name)
-    #filestr = File_to_Str(file_name)
+def process_log_dateobject (file_name, precision): #DateObject method
+    granulated_dateobj = date.today()
+    file_lines = file_to_list(file_name)
+    #filestr = file_to_str(file_name)
     pattern = r'(\d+\.\d+\.\d+\.\d+) - - \[(.*?)\] (.*)' 
-    for logLine in fileLines:
+    for log_line in file_lines:
         #print (logLine)
-        match = re.match(pattern, logLine)    
+        match = re.match(pattern, log_line)
         if match:
             ip, date_time, other = match.groups()
             #print (ip, '\n', date_time, '\n', other, '\n',)                    #DEBUG   
@@ -66,24 +69,24 @@ def ProcessLogDateObject (file_name, precision): #DateObject method
             #print ('date_time', date_time, '\n date_format ', date_format, )   #DEBUG
             date_obj = datetime.strptime(date_time, date_format)            
             if precision == 'sec':
-                granulated_dateObj = date_obj
+                granulated_dateobj = date_obj
             if precision == 'min':
-                granulated_dateObj = date_obj.replace(second=0)
+                granulated_dateobj = date_obj.replace(second=0)
             if precision == 'hour':
-                granulated_dateObj = date_obj.replace(minute=0, second=0)
+                granulated_dateoj = date_obj.replace(minute=0, second=0)
             #print('\n granulated_dateObj', granulated_dateObj, '\n')           #DEBUG
-            log_dict[granulated_dateObj] = ip+other
-            log_counts[granulated_dateObj] += 1
+            log_dict[granulated_dateobj] = ip+other
+            log_counts[granulated_dateobj] += 1
 
 def main():
     #print('LogParser usage: \n    1-st param to specify a log file, \n    2-nd parameter: granularity. Valid values: sec/min/hour \n    3-rd parameter "RegExp" or "DT" to specify regexp method or DateTime objects. Default: regexp \n Example: logReader.py access.log min DT')
 
     if method == 'DT':
         print ('\n Using DateTimeObjects: \n')
-        ProcessLogDateObject(log_file_name, granularity)
-    else:
+        process_log_dateobject(log_file_name, granularity)
+    if method == 'RegExp':
         print ('\n Using RegExp: \n')
-        ProcessLogRegExp(log_file_name, granularity)
+        process_log_regexp(log_file_name, granularity)
     #print('Dict: ', log_dict, '\n')                                             #DEBUG
     print ('Counter ', log_counts)    
 
